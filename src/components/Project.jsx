fd7ec8e761ffe9ea0ba887fac29ce9285370c401
Dhiraj <thakredheeraj02@gmail.com>
2025-09-18 19:19:31 +0530

    add Links and image

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,7 @@ import mpP from "../assets/Mp.png";
 import javaP from "../assets/javaP.png";
 import docP from "../assets/DocsP.png";
 import protP from "../assets/port.png";
+import fresh from "../assets/fresh.png";
 import Card from "./Card";
 
 function Project() {
@@ -13,7 +14,7 @@ function Project() {
       des: "Created a responsive and informative webpage highlighting the National Parks of Madhya Pradesh.",
       image: mpP,
       live: "https://mpwild-life.netlify.app/",
-      github: "",
+      github: "https://github.com/Thakreji02/Mp-WildLife.git",
       show: "on",
       status: "done",
     },
@@ -22,7 +23,7 @@ function Project() {
       des: "Built an interactive document management UI where users can upload, download, and manage data(Dummy).",
       image: docP,
       live: "https://docs-flow-interface.netlify.app/",
-      github: "github",
+      github: "https://github.com/Thakreji02/Document-Management-Interface-DocFlow.git",
       show: "on",
       status: "done",
     },
@@ -31,16 +32,16 @@ function Project() {
       des: "Welcome to my portfolio! I am an MCA graduate skilled in Java, Python, SQL, and web development, showcasing my projects and passion for technology.",
       image: protP,
       live: "https://my-portfolio-dhiraj.netlify.app/",
-      github: "github",
+      github: "https://github.com/Thakreji02/My-Portfolio.git",
       show: "on",
       status: "done",
     },
     {
       title: "Fresh Mart",
       des: "Welcome to our Mart Shop – a place where you can explore daily essentials and quality products all in one place. Enjoy smart shopping with great deals and convenience.",
-      image: docP,
-      live: "live",
-      github: "https://fresh-mart-groweb.netlify.app/",
+      image: fresh,
+      live: "https://fresh-mart-groweb.netlify.app/",
+      github: "https://github.com/Thakreji02/Fresh-Mart.git",
       show: "on",
       status: "pending",
     },
